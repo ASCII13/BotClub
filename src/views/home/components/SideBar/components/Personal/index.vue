d8f0640293e6c09f96ee532504bc03e4bf7a357a
ascii13 <476747909@qq.com>
2020-10-14 18:18:23 +0800

    Package SideBar finished

---
+++ src/views/home/components/SideBar/components/Personal/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <el-card shadow="hover" class="personal-panel">
+        <div slot="header">
+            <span>个人信息</span>
+        </div>
+        <div v-for="(item, index) in datas" :key="index" class="item-container">
+            <router-link :to="item.path">{{ item.title }}</router-link>
+        </div>
+    </el-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            datas: [
+                {
+                    title: '积分详情',
+                    path: '/ranking-list',
+                },
+                {
+                    title: '收藏文章',
+                    path: '/login',
+                },
+                {
+                    title: '代办事项',
+                    path: '/login',
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.personal-panel {
+    width: 300px;
+
+    .item-container {
+        padding: 5px 10px;
+        text-align: center;
+        font-size: 15px;
+    }
+}
+</style>
---
+++ src/views/home/components/SideBar/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="side-bar-container">
+        <personal-info></personal-info>
+        <commonly-used-site></commonly-used-site>
+    </div>
+</template>
+
+<script>
+import PersonalInfo from './components/Personal'
+import CommonlyUsedSite from './components/CommonlyUsedSite'
+
+export default {
+    components: {
+        PersonalInfo,
+        CommonlyUsedSite,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.side-bar-container {
+    display: flex;
+    flex-direction: column;
+
+    & > :not(:first-child) {
+        margin-top: 16px;
+    }
+}
+</style>
