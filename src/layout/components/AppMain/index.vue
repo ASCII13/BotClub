60058e8f5b9293df6a34bc9061f7517a85b5a516
ascii13 <476747909@qq.com>
2020-11-05 16:37:37 +0800

    Bug fix: login and register page show navigationBar

---
+++ src/layout/components/AppMain/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main-container">
+        <router-view></router-view>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.main-container {
+    padding: 0 16px;
+}
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <top-bar></top-bar>
+        <div class="middle-container">
+            <nav-bar v-if="show"></nav-bar>
+            <app-main></app-main>
+        </div>
+        <footer-bar></footer-bar>
+    </div>
+</template>
+
+<script>
+import TopBar from './components/TopBar'
+import AppMain from './components/AppMain'
+import NavBar from './components/NavigationBar'
+import FooterBar from './components/Footer'
+
+export default {
+    components: {
+        TopBar,
+        AppMain,
+        NavBar,
+        FooterBar,
+    },
+    computed: {
+        show() {
+            if (this.$route.path === '/login/index' || this.$route.path === '/register/index') {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.middle-container {
+    width: 100%;
+    min-height: calc(100vh - 56px - 200px);
+    padding: 16px;
+    display: flex;
+    justify-content: center;
+}
+</style>
