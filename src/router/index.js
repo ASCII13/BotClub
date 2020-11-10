fb053bd615871557e0103e3a99741663ce7710df
ascii13 <476747909@qq.com>
2020-11-10 18:16:03 +0800

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,184 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/layout'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: 'home',
+        component: () => import('@/views/home'),
+        name: 'Home',
+        meta: { title: '首页', active: true }
+      }
+    ]
   },
   {
     path: '/login',
-    name: 'Login',
-    component: () => import('@/views/login')
+    component: Layout,
+    hidden: true,
+    redirect: '/login/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/login'),
+        name: 'Login',
+        meta: { title: '登录' }
+      }
+    ]
   },
   {
     path: '/register',
-    name: 'Register',
-    component: () => import('@/views/register')
+    component: Layout,
+    hidden: true,
+    redirect: '/register/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/register'),
+        name: 'Register',
+        meta: { title: '注册' }
+      }
+    ]
+  },
+  {
+    path: '/square',
+    component: Layout,
+    redirect: '/square/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/square'),
+        name: 'Square',
+        meta: { title: '广场' }
+      }
+    ]
+  },
+  {
+    path: '/site-list',
+    component: Layout,
+    redirect: '/site-list/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/site-list'),
+        name: 'SiteList',
+        meta: { title: '导航' }
+      }
+    ]
+  },
+  {
+    path: '/questions',
+    component: Layout,
+    redirect: '/questions/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/questions'),
+        name: 'Questions',
+        meta: { title: '问答' }
+      }
+    ]
+  },
+  {
+    path: '/knowledge',
+    component: Layout,
+    redirect: '/knowledge/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/knowledge'),
+        name: 'Knowledge',
+        meta: { title: '体系' }
+      }
+    ]
+  },
+  {
+    path: '/project-list',
+    component: Layout,
+    redirect: '/project-list/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/project-list'),
+        name: 'ProjectList',
+        meta: { title: '项目' }
+      }
+    ]
+  },
+  {
+    path: '/wechat-articles',
+    component: Layout,
+    redirect: '/wechat-articles/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/wechat-articles'),
+        name: 'WeChatArticles',
+        meta: { title: '公众号' }
+      }
+    ]
+  },
+  {
+    path: '/favorite-list',
+    component: Layout,
+    hidden: true,
+    redirect: '/favorite-list/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/favorite-list'),
+        name: 'FavoriteList',
+        meta: { title: '收藏列表' }
+      }
+    ]
+  },
+  {
+    path: '/ranking-list',
+    component: Layout,
+    hidden: true,
+    redirect: '/ranking-list/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/ranking-list'),
+        name: 'RankingList',
+        meta: { title: '排行榜' }
+      }
+    ]
+  },
+  {
+    path: '/search-result',
+    component: Layout,
+    hidden: true,
+    redirect: '/search-result/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/search-result'),
+        name: 'SearchResult',
+        meta: { title: '搜索结果' }
+      }
+    ]
+  },
+  {
+    path: '/todo',
+    component: Layout,
+    hidden: true,
+    redirect: '/todo/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/todo'),
+        name: 'Todo',
+        meta: { title: '待办事项' }
+      }
+    ]
   }
 ]
 
