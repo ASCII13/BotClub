c341c27c632e39d94b18d444bbef6ab7f27f5f80
ascii13 <476747909@qq.com>
2020-11-12 11:17:34 +0800

    add logo and optimized some css

--- src/layout/components/TopBar/index.vue
+++ src/layout/components/TopBar/index.vue
@@ -1,16 +1,12 @@
 <template>
     <div class="top-bar-container">
-        <div class="logo">
-            <!-- <div class="logo-container">
-                <span class="text">V</span>
-                <span class="text">A</span>
-            </div> -->
-        </div>
+        <div class="logo" @click="backToHome">VA</div>
         <el-input
             clearable
-            placeholder="搜索..."
+            placeholder="多个关键字用空格隔开..."
             class="search-bar"
-            v-model="searchContent">
+            v-model="searchContent"
+            @change="search">
         </el-input>
         <div class="right-actions">
             <div v-if="!name">
@@ -21,7 +17,7 @@
             <div v-else>
                 <el-dropdown @command="handleCommand">
                     <span class="nickname">{{ name }}
-                        <i class="el-icon-arrow-down"></i>
+                        <i class="el-icon-caret-bottom"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
                         <el-dropdown-item command="logout">退出登录</el-dropdown-item>
@@ -33,7 +29,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex';
 
 export default {
     data() {
@@ -51,16 +47,23 @@ export default {
             switch (command) {
                 case "logout":
                     this.$store.dispatch('user/logout').then(() => {
-                        this.$message({
-                            type: 'success',
-                            message: '已退出登录'
-                        })
+                        this.$message.success('已退出登录');
                     })
                     break;
             
                 default:
                     break;
             }
+        },
+        backToHome() {
+            this.$router.push('/');
+        },
+        search() {
+            if (!this.searchContent) return;
+            this.$store.dispatch('app/setSearchKey', this.searchContent);
+
+            if (this.$route.path === '/search-result/index') return;
+            this.$router.push('/search-result');
         }
     }
 }
@@ -69,46 +72,35 @@ export default {
 <style lang="scss" scoped>
 .top-bar-container {
     height: 56px;
-    box-sizing: border-box;
-    padding: 10px 260px; 
     display: flex;
     align-items: center;
     background-color: #ffffff;
     box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.1);
 
     .logo {
+        margin-left: auto;
         width: 50px;
-        height: 40px;
+        line-height: 40px;
         text-align: center;
-        background-color: #eeeeee;
-
-        // .logo-container {
-        //     line-height: 40px;
-        //     text-align: center;
-        //     background-color: #eeeeee;
-        // }
-
-        .text {
-            font-size: 22px;
-            line-height: 40px;
-        }
+        background-color: black;
+        color: white;
+        font-family: Oswald;
+        font-size: 28px;
+        border-radius: 3px;
     }
 
     .search-bar {
         max-width: 420px;
-        flex: 1 auto;
-        margin: 0 1rem;
+        margin-left: 1rem;
+        margin-right: auto;
     }
 
     .right-actions {
-        display: flex;
-        align-items: center;
-        margin-left: auto;
+        margin-right: auto;
 
         .nickname {
-            color: #3850d7;
+            font-weight: 500;
         }
     }
-
 }
 </style>
