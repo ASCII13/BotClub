0c0b0a637800c30fd4689e6a8769f181c0eb7376
ascii13 <476747909@qq.com>
2021-09-16 17:01:29 +0800

    optimized vision of ex-link

--- src/components/ExLink/index.vue
+++ src/components/ExLink/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <a :href="href" :target="target"><slot/></a>
+    <a v-if="!router" :href="href" :target="target" :class="{'hyperlinks': underlined}"><slot/></a>
+    <router-link v-else :class="{'hyperlinks': underlined}" :to="href"><slot/></router-link>
 </template>
 
 <script>
@@ -14,6 +15,28 @@ export default {
             type: String,
             default: '_blank'
         },
+        underlined: {
+            type: Boolean,
+            default: true
+        },
+        router: {
+            type: Boolean,
+            default: false
+        }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.hyperlinks {
+    background: linear-gradient(90deg, #69a530, #69a530);
+    background-size: 0 0.3rem;
+    background-repeat: no-repeat;
+    background-position: 0 100%;
+    transition: 0.2s all;
+
+    &:hover {
+        background-size: 100% 0.3rem;
+    }
+}
+</style>
