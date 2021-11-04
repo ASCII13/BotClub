adc50c6cd2369eb5cb3fb79ad54329403726e811
ascii13 <476747909@qq.com>
2021-11-04 15:35:18 +0800

    improve user interface

--- src/views/site-list/index.vue
+++ src/views/site-list/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="site-container">
-        <list-view class="site-list" :loading="loading">
+    <div style="display: flex;">
+        <list-view style="width: 600px;" :loading="loading">
             <el-card v-for="type in siteList" :key="type.cid" :id="'id'+type.cid" class="type-item">
                 <div slot="header">{{ type.name }}</div>
                 <div class="item-list">
@@ -16,7 +16,7 @@
             </el-card>
         </list-view>
         <el-card class="catagroies" v-loading="loading" :body-style="{ 'display': 'flex', 'flex-direction': 'column',}">
-            <div v-for="type in siteList" :key="type.cid" @click="scrollIntoElem(type.cid)" class="catagroy-item">
+            <div v-for="type in siteList" :key="type.cid" @click="currAnchor = '#id' + type.cid" class="catagroy-item">
                 <ex-link>{{ type.name }}</ex-link>
             </div>
         </el-card>
@@ -27,75 +27,77 @@
 import ExLink from '@/components/ExLink';
 import ListView from '@/components/ListView';
 import { getSiteList } from '@/api/sites';
-
 export default {
     data() {
         return {
             siteList: [],
-            loading: false,
+            loading: true,
+            currAnchor: '',
         }
     },
     created() {
-        this.loading = true;
         getSiteList().then(res => {
             this.loading = false;
-            let datas = res.data;
-            if (datas && datas.length != 0) {
-                this.siteList = datas.filter(item => {
-                    return item.articles.length > 0;
-                });
-            }
-        })
+
+            const datas = res.data;
+            if (!datas || datas.length === 0) return;
+
+            this.siteList = datas.filter(item => item.articles.length > 0);
+        });
     },
-    methods: {
-        scrollIntoElem(id) {
-            const view = document.querySelector('#id'+id);
-            if (view) {
-                view.scrollIntoView();
-            }
-        }
-    },
+    // methods: {
+    //     scrollIntoElem(id) {
+    //         const view = document.querySelector('#id'+id);
+    //         if (view) {
+    //             console.log(view);
+    //             view.style.border = '1px solid red';
+    //             view.scrollIntoView();
+    //         }
+    //     }
+    // },
     components: {
         ListView,
         ExLink,
+    },
+    watch: {
+        currAnchor(newVal, oldVal) {
+            if (oldVal) {
+                const pre = document.querySelector(oldVal);
+                pre.classList.toggle('anchor');
+            }
+            const next = document.querySelector(newVal);
+            next.classList.toggle('anchor');
+            next.scrollIntoView();
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-.site-container {
+.type-item:not(:last-child) {
+    margin-bottom: 6px;
+}
+.item-list {
     display: flex;
-
-    .site-list {
-        width: 600px;
-
-        .type-item:not(:last-child) {
-            margin-bottom: 6px;
-        }
-
-        .item-list {
-            display: flex;
-            flex-wrap: wrap;
-        }
+    flex-wrap: wrap;
+}
+.catagroies {
+    width: 200px;
+    height: 500px;
+    overflow: auto;
+    margin-left: 0.6rem;
+    &:hover {
+        cursor: pointer;
     }
-
-    .catagroies {
-        width: 200px;
-        height: 500px;
-        overflow: auto;
-        margin-left: 0.6rem;
-        &:hover {
-            cursor: pointer;
-        }
-
-        .catagroy-item {
-            margin: 8px 0;
-            text-align: center;
-        }
+    .catagroy-item {
+        margin: 8px 0;
+        text-align: center;
     }
-
-    .item {
-        padding: 10px;
-    }
+}
+.item {
+    padding: 10px;
+}
+.anchor {
+    border-color: $primaryColor;
 }
 </style>
