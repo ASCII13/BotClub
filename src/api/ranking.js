c42a09c0848a7b34840089f56220e88fae0511e9
ascii13 <476747909@qq.com>
2021-11-19 17:46:40 +0800

    optimize code

--- src/api/ranking.js
+++ src/api/ranking.js
@@ -1,20 +1,20 @@
 import request from '@/utils/request'
 
-export function getRankingList(query = 1) {
+export function fetchRankingList(query = 1) {
     return request({
         url: `/coin/rank/${query}/json`,
         method: 'get'
     })
 }
 
-export function getSelfRankingInfo() {
+export function fetchSelfRankingInfo() {
     return request({
         url: '/lg/coin/userinfo/json',
         method: 'get'
     })
 }
 
-export function getSelfPointsInfo(query = 1) {
+export function fetchSelfPointsInfo(query = 1) {
     return request({
         url: `/lg/coin/list/${query}/json`,
         method: 'get'
--- src/views/ranking-list/index.vue
+++ src/views/ranking-list/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <list-view :busy="busy" :noMore="noMore" :showHint="showHint" :loading="loading" :more="more" style="width: 660px;">
+    <list-view :busy="busy" :no-more="noMore" :show-hint="showHint" :loading="loading" :more="more" style="width: 660px;">
         <el-card
-            v-for="user in userList"
+            v-for="user in users"
             :key="user.userId"
             :body-style="{ 'display': 'flex', 'align-items': 'center', 'height': '80px', 'padding': '15px' }"
             class="user-item">
-            <el-avatar class="avatar" :style="{ 'background-color': theme }">{{ getFirstChar(user.username) }}</el-avatar>
+            <avatar :name="user.username"></avatar>
             <div class="username">{{ user.username }}</div>
             <div class="coin">积分：{{ user.coinCount }}</div>
             <div class="level">等级：{{ user.level }}</div>
@@ -15,64 +15,58 @@
 </template>
 
 <script>
+import Avatar from '@/components/Avatar';
 import ListView from '@/components/ListView';
-import { getFirstChar } from '@/utils/text';
-import { getRankingList } from '@/api/ranking';
-import { mapGetters } from "vuex";
-
+import { fetchRankingList } from '@/api/ranking';
 export default {
     data() {
         return {
             busy: false,
             noMore: false,
-            showHint: false,
-            loading: false,
-            currPage: 1,
-            userList: [],
+            loading: true,
+            pageNum: 1,
+            users: [],
         }
     },
     created() {
-        this.getRankingData('init', this.currPage);
+        this.getUsers('init', this.pageNum);
     },
     components: {
+        Avatar,
         ListView,
     },
     methods: {
-        getFirstChar,
-        more() {
-            this.getRankingData('more', this.currPage);
-        },
-        getRankingData(state, page) {
-            if (state === 'init') {
-                this.loading = true;
-                getRankingList(page).then(res => {
-                    this.loading = false;
-                    if (res.data.datas != undefined && res.data.datas.length != 0) {
-                        this.userList = res.data.datas;
-                        this.currPage = page + 1;
+        getUsers(type, pageNum) {
+            this.busy = type !== 'init';
+            fetchRankingList(pageNum).then(res => {
+                this.loading = false;
+
+                const data = res.data;
+                const rankings = data.datas;
+                if (data && rankings && rankings.length > 0) {
+                    if (type === 'init') {
+                        this.users = rankings;
                     } else {
-                        this.showHint = true;
+                        this.users.push(...rankings);
+                        this.busy = false;
                     }
-                })
-            }
-            if (state === 'more') {
-                this.busy = true;
-                getRankingList(page).then(res => {
-                    this.busy = false;
-                    if (res.data.datas != undefined && res.data.datas.length != 0) {
-                        this.userList.push(...res.data.datas);
-                        this.currPage = page + 1;
-                    } else {
+                    this.pageNum = ++pageNum;
+                } else {
+                    if (type !== 'init') {
                         this.noMore = true;
                     }
-                })
-            }
-        }
+                }
+            });
+        },
+        more() {
+            this.getUsers('more', this.pageNum);
+        },
     },
     computed: {
-        ...mapGetters([
-            'theme',
-        ])
+        showHint() {
+            return !this.loading &&
+                (!this.users || this.users.length === 0);
+        }
     }
 }
 </script>
@@ -82,28 +76,23 @@ export default {
     &:not(:first-child) {
         margin-top: 6px;
     }
-
     & > div {
         font-size: 15px;
     }
 }
-
 .avatar {
     width: 45px;
     height: 45px;
 }
-
 .username {
     margin-left: 0.8rem;
 }
-
 .username::after,
 .coin::after {
     content: '|';
     padding: 0 0.8rem;
     color: #909399;
 }
-
 .rank {
     margin-left: auto;
 }
