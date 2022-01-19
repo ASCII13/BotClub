e22bbe29aa6451662ca336cfbf0bbdb3b499877f
wangzhiwen <wangzhiwen@51tiangou.com>
2022-01-19 17:36:02 +0800

    add dark mode switch component

---
+++ src/layout/components/TopBar/ModeSwitch.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="dark-mode-switch" @click="dark = !dark" :class="{'dark': dark}">
+        <div class="inner">
+            <div class="bg"/>
+            <div class="circle"/>
+        </div>
+        <div class="rays">
+            <div class="ray" v-for="count in 8" :key="count"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            dark: false
+        }
+    },
+}
+</script>
+
+<style lang="scss" scopeds>
+.dark-mode-switch {
+    position: relative;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    cursor: pointer;
+
+    &:hover {
+        .circle {
+            margin: {
+                top: -1.25rem;
+                left: .3rem;
+            }
+        }
+    }
+    .inner {
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+        overflow: hidden;
+    }
+    .bg {
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+        background-color: #202020;
+        border: 1px solid #f0f0f0;
+        transition: all 0s;
+    }
+    .circle {
+        margin: {
+            top: -1.3rem;
+            left: .35rem;
+        };
+        width: 1rem;
+        height: 1rem;
+        border-radius: 50%;
+        background-color: #f0f0f0;
+        border: 2px solid transparent;
+        transition: all 0.2s;
+    }
+    .rays {
+        position: absolute;
+        left: 0;
+        top: 0;
+
+        .ray {
+            position: absolute;
+            top: .5rem;
+            left: .4rem;
+            width: 3px;
+            height: 4px;
+            background-color: #fbc02d;
+            transform-origin: 2px 0;
+            opacity: 0;
+
+            @for $i from 1 through 8 {
+                &:nth-child(#{$i}) {
+                    transform: rotate(-45deg * $i - 135deg) translateY(11px);
+                    transition: all 0.2s;
+                }
+            }
+        }
+    }
+    &.dark {
+        &:hover {
+            .rays .ray {
+                @for $i from 1 through 8 {
+                    &:nth-child(#{$i}) {
+                        transform: rotate(-45deg * $i - 135deg) translateY(13px);
+                    }
+                } 
+            }
+        }
+        .circle {
+            margin-left: 0;
+            margin-top: -1rem;
+            background-color: #fbc02d;
+            border-color: #f9a825;
+        }
+        .rays .ray {
+            opacity: 1;
+
+            @for $i from 1 through 8 {
+                &:nth-child(#{$i}) {
+                    transform: rotate(-45deg * $i - 135deg) translateY(11px);
+                    transition: all (0.2s + $i * 0.2s);
+                }
+            } 
+        }
+    }
+}
+</style>
